b3c6eddf781d4cf9988b86f09e6c2c6e70aa8da4
Kuba Szulaczkowski <kuba.szulaczkowski@gmail.com>
2023-03-27 01:12:32 -0400

    Fix

--- NYCSchoolsChallenge/NYCSchoolsChallengeApp.swift
+++ NYCSchoolsChallenge/NYCSchoolsChallengeApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct NYCSchoolsChallengeApp: App {
     var body: some Scene {
         WindowGroup {
-            SchoolListScreen(schools: School.sampleData, results: TestResults.sampleData)
+            SchoolListScreen()
         }
     }
 }
