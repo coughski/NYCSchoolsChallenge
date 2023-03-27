c0ec5941135fe5a28aca5cc927e5e1fe50178066
Kuba Szulaczkowski <kuba.szulaczkowski@gmail.com>
2023-03-26 21:57:18 -0400

    Initial Commit

---
+++ NYCSchoolsChallenge/NYCSchoolsChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NYCSchoolsChallengeApp.swift
+//  NYCSchoolsChallenge
+//
+//  Created by Kuba Szulaczkowski on 3/26/23.
+//
+
+import SwiftUI
+
+@main
+struct NYCSchoolsChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
