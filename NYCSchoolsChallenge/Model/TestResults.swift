867a27e0df6e60433acef0733c9fc567d380e109
Kuba Szulaczkowski <kuba.szulaczkowski@gmail.com>
2023-03-27 01:48:54 -0400

    Remove redundant model field

--- NYCSchoolsChallenge/Model/TestResults.swift
+++ NYCSchoolsChallenge/Model/TestResults.swift
@@ -9,7 +9,6 @@ typealias TestResultsResponse = [TestResults]
 
 struct TestResults {
     let dbn: String
-    let schoolName: String
     let numOfSatTestTakers: Int?
     
     let satCriticalReadingAvgScore: Int?
@@ -19,13 +18,12 @@ struct TestResults {
 
 extension TestResults: Decodable {
     enum CodingKeys: CodingKey {
-        case dbn, schoolName, numOfSatTestTakers, satCriticalReadingAvgScore, satMathAvgScore, satWritingAvgScore
+        case dbn, numOfSatTestTakers, satCriticalReadingAvgScore, satMathAvgScore, satWritingAvgScore
     }
     
     init(from decoder: Decoder) throws {
         let values = try decoder.container(keyedBy: CodingKeys.self)
         dbn = try values.decode(String.self, forKey: .dbn)
-        schoolName = try values.decode(String.self, forKey: .schoolName)
         numOfSatTestTakers = Int(try values.decode(String.self, forKey: .numOfSatTestTakers))
         
         satCriticalReadingAvgScore = Int(try values.decode(String.self, forKey: .satCriticalReadingAvgScore))
@@ -36,8 +34,8 @@ extension TestResults: Decodable {
 
 extension TestResults {
     static let sampleData = [
-        TestResults(dbn: "21K728", schoolName: "LIBERATION DIPLOMA PLUS", numOfSatTestTakers: 10, satCriticalReadingAvgScore: 411, satMathAvgScore: 369, satWritingAvgScore: 373),
-        TestResults(dbn: "08X282", schoolName: "WOMEN'S ACADEMY OF EXCELLENCE", numOfSatTestTakers: 44, satCriticalReadingAvgScore: 407, satMathAvgScore: 386, satWritingAvgScore: 378)
+        TestResults(dbn: "21K728", numOfSatTestTakers: 10, satCriticalReadingAvgScore: 411, satMathAvgScore: 369, satWritingAvgScore: 373),
+        TestResults(dbn: "08X282", numOfSatTestTakers: 44, satCriticalReadingAvgScore: 407, satMathAvgScore: 386, satWritingAvgScore: 378)
     ]
     
     static let sampleJSON = """
