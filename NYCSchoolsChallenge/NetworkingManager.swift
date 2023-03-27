01d9795af02fb65ed4cf500dd47a453f82930ea3
Kuba Szulaczkowski <kuba.szulaczkowski@gmail.com>
2023-03-27 18:27:19 -0400

    Fix warning

--- NYCSchoolsChallenge/NetworkingManager.swift
+++ NYCSchoolsChallenge/NetworkingManager.swift
@@ -15,7 +15,7 @@ final class NetworkingManager {
     func request<T: Decodable>(_ absoluteURL: String) async throws -> T {
         guard let url = URL(string: absoluteURL) else { throw NetworkingError.invalidURL }
         
-        var request = URLRequest(url: url)
+        let request = URLRequest(url: url)
         let response: (Data, URLResponse)
         do {
             response = try await URLSession.shared.data(for: request)
