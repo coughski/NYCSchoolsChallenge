4d2436ffb2ce3ba8fea24bdaecb530e96465076a
Kuba Szulaczkowski <kuba.szulaczkowski@gmail.com>
2023-03-27 01:28:11 -0400

    Improve networking error handling

--- NYCSchoolsChallenge/NetworkingManager.swift
+++ NYCSchoolsChallenge/NetworkingManager.swift
@@ -15,17 +15,35 @@ final class NetworkingManager {
     func request<T: Decodable>(_ absoluteURL: String) async throws -> T {
         guard let url = URL(string: absoluteURL) else { throw NetworkingError.invalidURL }
         
-        let response = try await URLSession.shared.data(from: url)
+        let response: (Data, URLResponse)
+        do {
+            response = try await URLSession.shared.data(from: url)
+        } catch {
+            throw NetworkingError.custom(error: error)
+        }
+        
+        let httpResponse = response.1 as! HTTPURLResponse
+        guard (200...300) ~= httpResponse.statusCode else {
+            throw NetworkingError.invalidStatusCode(statusCode: httpResponse.statusCode)
+        }
         
         let jsonDecoder = JSONDecoder()
         jsonDecoder.keyDecodingStrategy = .convertFromSnakeCase
         
-        return try jsonDecoder.decode(T.self, from: response.0)
+        do {
+            let data = try jsonDecoder.decode(T.self, from: response.0)
+            return data
+        } catch {
+            throw NetworkingError.failedToDecode
+        }
     }
 }
 
 extension NetworkingManager {
     enum NetworkingError: Error {
         case invalidURL
+        case custom(error: Error)
+        case invalidStatusCode(statusCode: Int)
+        case failedToDecode
     }
 }
