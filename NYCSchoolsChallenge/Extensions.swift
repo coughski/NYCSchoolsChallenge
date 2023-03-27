1198b15af45bb9c1232d7422cfdc472b9a3e2e74
Kuba Szulaczkowski <kuba.szulaczkowski@gmail.com>
2023-03-27 18:30:31 -0400

    Add Color extension for color assets

---
+++ NYCSchoolsChallenge/Extensions.swift
@@ -0,0 +1,14 @@
+//
+//  Extensions.swift
+//  NYCSchoolsChallenge
+//
+//  Created by Kuba Szulaczkowski on 3/27/23.
+//
+
+import SwiftUI
+
+extension Color {
+    static let publicNavy = Color("publicNavy"),
+               nycGreen = Color("nycGreen"),
+               schoolOrange = Color("schoolOrange")
+}
