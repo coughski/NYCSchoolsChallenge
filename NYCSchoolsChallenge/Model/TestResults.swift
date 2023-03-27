34179dbb73f02cd0e8ede0c49c82bbcec56a6f3e
Kuba Szulaczkowski <kuba.szulaczkowski@gmail.com>
2023-03-26 23:47:07 -0400

    Add sample JSON

--- NYCSchoolsChallenge/Model/TestResults.swift
+++ NYCSchoolsChallenge/Model/TestResults.swift
@@ -39,4 +39,25 @@ extension TestResults {
         TestResults(dbn: "21K728", schoolName: "LIBERATION DIPLOMA PLUS", numOfSatTestTakers: 10, satCriticalReadingAvgScore: 411, satMathAvgScore: 369, satWritingAvgScore: 373),
         TestResults(dbn: "08X282", schoolName: "WOMEN'S ACADEMY OF EXCELLENCE", numOfSatTestTakers: 44, satCriticalReadingAvgScore: 407, satMathAvgScore: 386, satWritingAvgScore: 378)
     ]
+    
+    static let sampleJSON = """
+[
+    {
+        "dbn": "21K728",
+        "school_name": "LIBERATION DIPLOMA PLUS",
+        "num_of_sat_test_takers": "10",
+        "sat_critical_reading_avg_score": "411",
+        "sat_math_avg_score": "369",
+        "sat_writing_avg_score": "373"
+    },
+    {
+        "dbn": "08X282",
+        "school_name": "WOMEN'S ACADEMY OF EXCELLENCE",
+        "num_of_sat_test_takers": "44",
+        "sat_critical_reading_avg_score": "407",
+        "sat_math_avg_score": "386",
+        "sat_writing_avg_score": "378"
+    }
+]
+"""
 }
