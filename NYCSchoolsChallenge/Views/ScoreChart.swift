1238dc685e00a85e92809b2b9acc273dfab15894
Kuba Szulaczkowski <kuba.szulaczkowski@gmail.com>
2023-03-27 23:56:36 -0400

    Customize score chart

--- NYCSchoolsChallenge/Views/ScoreChart.swift
+++ NYCSchoolsChallenge/Views/ScoreChart.swift
@@ -9,13 +9,41 @@ import SwiftUI
 import Charts
 
 struct ScoreChart: View {
+    let scores: TestResults
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Chart {
+            BarMark(
+                x: .value("Score", scores.satCriticalReadingAvgScore ?? 0),
+                y: .value("Section", "Reading")
+            )
+            .foregroundStyle(Color.boardBlue)
+            
+            BarMark(
+                x: .value("Score", scores.satMathAvgScore ?? 0),
+                y: .value("Section", "Math")
+            )
+            .foregroundStyle(Color.nycGreen)
+            
+            BarMark(
+                x: .value("Score", scores.satWritingAvgScore ?? 0),
+                y: .value("Section", "Writing")
+            )
+            .foregroundStyle(Color.schoolOrange)
+        }
+        .chartXScale(domain: [0, 800])
+        .chartXAxis {
+            AxisMarks(values: .automatic(desiredCount: 8))
+        }
+        .chartYAxis {
+            AxisMarks(values: .automatic, stroke: StrokeStyle(lineWidth: 0))
+        }
+        .frame(height: 200)
     }
 }
 
 struct ScoreChart_Previews: PreviewProvider {
     static var previews: some View {
-        ScoreChart()
+        ScoreChart(scores: TestResults.sampleData[0])
     }
 }
