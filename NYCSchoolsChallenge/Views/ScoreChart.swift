469565e7f3b3af93ff6f322d656d9b042923875e
Kuba Szulaczkowski <kuba.szulaczkowski@gmail.com>
2023-03-27 22:42:06 -0400

    Add stub chart view

---
+++ NYCSchoolsChallenge/Views/ScoreChart.swift
@@ -0,0 +1,21 @@
+//
+//  ScoreChart.swift
+//  NYCSchoolsChallenge
+//
+//  Created by Kuba Szulaczkowski on 3/27/23.
+//
+
+import SwiftUI
+import Charts
+
+struct ScoreChart: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ScoreChart_Previews: PreviewProvider {
+    static var previews: some View {
+        ScoreChart()
+    }
+}
