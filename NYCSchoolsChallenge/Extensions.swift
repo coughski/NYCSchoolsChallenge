bc228f677c370d868bf8936daeb6b5bf7768e4b0
Kuba Szulaczkowski <kuba.szulaczkowski@gmail.com>
2023-03-27 22:14:16 -0400

    Add forgotten asset color

--- NYCSchoolsChallenge/Extensions.swift
+++ NYCSchoolsChallenge/Extensions.swift
@@ -10,5 +10,6 @@ import SwiftUI
 extension Color {
     static let publicNavy = Color("publicNavy"),
                nycGreen = Color("nycGreen"),
-               schoolOrange = Color("schoolOrange")
+               schoolOrange = Color("schoolOrange"),
+               boardBlue = Color("boardBlue")
 }
