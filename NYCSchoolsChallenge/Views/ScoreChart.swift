ad95cc741244d08a6632d7b25ed9da24bcc80a96
Kuba Szulaczkowski <kuba.szulaczkowski@gmail.com>
2023-03-28 00:50:41 -0400

    Improve score chart

--- NYCSchoolsChallenge/Views/ScoreChart.swift
+++ NYCSchoolsChallenge/Views/ScoreChart.swift
@@ -18,25 +18,62 @@ struct ScoreChart: View {
                 y: .value("Section", "Reading")
             )
             .foregroundStyle(Color.boardBlue)
+            .cornerRadius(8, style: .continuous)
+            .annotation(position: .overlay, alignment: .trailing, spacing: 12) {
+                Text("Reading")
+                    .font(.system(.title3, design: .rounded))
+                    .bold()
+                    .foregroundColor(Color(uiColor: .systemBackground))
+            }
+            .annotation(position: .trailing, alignment: .trailing, spacing: 8) {
+                Text("\(scores.satCriticalReadingAvgScore ?? 0)")
+                    .font(.system(.title3, design: .rounded))
+                    .foregroundColor(.publicNavy)
+            }
             
             BarMark(
                 x: .value("Score", scores.satMathAvgScore ?? 0),
                 y: .value("Section", "Math")
             )
             .foregroundStyle(Color.nycGreen)
+            .cornerRadius(8, style: .continuous)
+            .annotation(position: .overlay, alignment: .trailing, spacing: 12) {
+                Text("Math")
+                    .font(.system(.title3, design: .rounded))
+                    .bold()
+                    .foregroundColor(Color(uiColor: .systemBackground))
+            }
+            .annotation(position: .trailing, alignment: .trailing, spacing: 8) {
+                Text("\(scores.satMathAvgScore ?? 0)")
+                    .font(.system(.title3, design: .rounded))
+                    .foregroundColor(.publicNavy)
+            }
             
             BarMark(
                 x: .value("Score", scores.satWritingAvgScore ?? 0),
                 y: .value("Section", "Writing")
             )
             .foregroundStyle(Color.schoolOrange)
+            .cornerRadius(8, style: .continuous)
+            .annotation(position: .overlay, alignment: .trailing, spacing: 12) {
+                Text("Writing")
+                    .font(.system(.title3, design: .rounded, weight: .bold))
+                    .foregroundColor(Color(uiColor: .systemBackground))
+            }
+            .annotation(position: .trailing, alignment: .trailing, spacing: 8) {
+                Text("\(scores.satWritingAvgScore ?? 0)")
+                    .font(.system(.title3, design: .rounded))
+                    .foregroundColor(.publicNavy)
+            }
         }
-        .chartXScale(domain: [0, 800])
+        .chartXScale(domain: [0, 800], range: .plotDimension(endPadding: 50))
         .chartXAxis {
             AxisMarks(values: .automatic(desiredCount: 8))
         }
         .chartYAxis {
-            AxisMarks(values: .automatic, stroke: StrokeStyle(lineWidth: 0))
+            AxisMarks(values: .automatic) {
+                AxisValueLabel(offsetsMarks: false) {}
+            }
         }
         .frame(height: 200)
     }
