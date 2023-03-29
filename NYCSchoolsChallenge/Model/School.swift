d2ba98bc85b96d273d367f2869156848a7184496
Kuba Szulaczkowski <kuba.szulaczkowski@gmail.com>
2023-03-28 22:12:11 -0400

    Sanitize school data fields like overview and website

--- NYCSchoolsChallenge/Model/School.swift
+++ NYCSchoolsChallenge/Model/School.swift
@@ -37,10 +37,17 @@ extension School: Decodable {
         dbn = try values.decode(String.self, forKey: .dbn)
         name = try values.decode(String.self, forKey: .name)
         
-        overview = try? values.decode(String.self, forKey: .overview)
+        overview = (try? values.decode(String.self, forKey: .overview))?.replacingOccurrences(of: "Â", with: "")
+        
         phoneNumber = try? values.decode(String.self, forKey: .phoneNumber)
         email = try? values.decode(String.self, forKey: .email)
-        website = try? values.decode(String.self, forKey: .website)
+        
+        var rawWebsite = try? values.decode(String.self, forKey: .website)
+        if let rawWebsite {
+            website = !rawWebsite.hasPrefix("http") ? "http://" + rawWebsite : rawWebsite
+        } else {
+            website = nil
+        }
         
         totalStudents = Int(try values.decode(String.self, forKey: .totalStudents))
         graduationRate = Double((try? values.decode(String.self, forKey: .graduationRate)) ?? "")
@@ -54,6 +61,6 @@ extension School: Decodable {
 extension School {
     static let sampleData = [
         School(dbn: "21K728", name: "Liberation Diploma Plus High School", overview: "", phoneNumber: "", email: "", website: "", totalStudents: 206, graduationRate: nil, attendanceRate: nil, latitude: nil, longitude: nil, borough: "BROOKLYN"),
-        School(dbn: "08X282", name: "Women's Academy of Excellence", overview: "The WomenÂ’s Academy of Excellence is an all-girls public high school, serving grades 9-12. Our mission is to create a community of lifelong learners, to nurture the intellectual curiosity and creativity of young women and to address their developmental needs. The school community cultivates dynamic, participatory learning, enabling students to achieve academic success at many levels, especially in the fields of math, science, and civic responsibility. Our scholars are exposed to a challenging curriculum that encourages them to achieve their goals while being empowered to become young women and leaders. Our Philosophy is GIRLS MATTER!", phoneNumber: "718-542-0740", email: "sburns@schools.nyc.gov", website: "schools.nyc.gov/SchoolPortals/08/X282", totalStudents: 338, graduationRate: 0.612999976, attendanceRate: nil, latitude: nil, longitude: nil, borough: "BRONX    ")
+        School(dbn: "08X282", name: "Women's Academy of Excellence", overview: "The Women’s Academy of Excellence is an all-girls public high school, serving grades 9-12. Our mission is to create a community of lifelong learners, to nurture the intellectual curiosity and creativity of young women and to address their developmental needs. The school community cultivates dynamic, participatory learning, enabling students to achieve academic success at many levels, especially in the fields of math, science, and civic responsibility. Our scholars are exposed to a challenging curriculum that encourages them to achieve their goals while being empowered to become young women and leaders. Our Philosophy is GIRLS MATTER!", phoneNumber: "718-542-0740", email: "sburns@schools.nyc.gov", website: "schools.nyc.gov/SchoolPortals/08/X282", totalStudents: 338, graduationRate: 0.612999976, attendanceRate: nil, latitude: nil, longitude: nil, borough: "BRONX    ")
     ]
 }
