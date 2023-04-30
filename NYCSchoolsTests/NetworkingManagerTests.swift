f534bf18a1ae361a4de20535e09c8260d108f298
Kuba Szulaczkowski <kuba.szulaczkowski@gmail.com>
2023-04-29 22:29:06 -0400

    Create stub for NetworkingManager unit tests

---
+++ NYCSchoolsTests/NetworkingManagerTests.swift
@@ -0,0 +1,16 @@
+//
+//  NetworkingManagerTests.swift
+//  NYCSchoolsTests
+//
+//  Created by Kuba Szulaczkowski on 4/29/23.
+//
+
+import Foundation
+import XCTest
+@testable import class NYCSchoolsChallenge.NetworkingManager
+
+class NetworkingManagerTests: XCTestCase {
+    func testSuccessfulValidResponse() {
+        XCTFail()
+    }
+}
